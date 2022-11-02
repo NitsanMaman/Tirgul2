5a92ffaaff113d2505ec5acf18f740553def4a65
ronshahar6@gmail.com <Ron@Ron-Laptop.local>
2022-11-02 13:46:01 +0200

    replaced int [] array with ArrayList<Integer>

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TestThreadCheckArray {
@@ -6,11 +7,12 @@ public class TestThreadCheckArray {
 			Thread thread1, thread2;
 			System.out.println("Enter array size");
 			int num  = input.nextInt();
-			int [] array = new int[num];
+			//int [] array = new int[num];
+			ArrayList<Integer> array= new ArrayList<Integer>(num);
 			System.out.println("Enter numbers for array");
 			
 			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+				array.add(input.nextInt());
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
